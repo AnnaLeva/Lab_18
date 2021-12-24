8d915af27ddf9d5ada0029b2cb99d7b07f42c61a
Анастасия Александровна Ковалева <309755@niuitmo.ru>
2021-12-24 21:28:42 +0300

    Добавьте файлы проекта.

---
+++ Lab_18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            foreach (int l in list)
+            {
+                Console.WriteLine(l);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_18_1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_18_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The prase:");
+            char[] arrayS = Convert.ToString(Console.ReadLine()).ToArray();
+
+            Stack<char> stack = new Stack<char>();
+            stack.Push('0');
+            foreach (char i in arrayS)
+            {
+                if (i == '(')
+                {
+                    stack.Push(')');
+                }
+
+                if (i == '{')
+                {
+                    stack.Push('}');
+                }
+
+                if (i == '[')
+                {
+                    stack.Push(']');
+                }
+
+                if (i == stack.Peek())
+                {
+                    stack.Pop();
+                }
+            }
+
+            stack.Pop(); 
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Nice.");
+            }
+            else
+            {
+                Console.WriteLine("Not Good.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
